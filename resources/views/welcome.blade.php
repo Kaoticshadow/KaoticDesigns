b234e05f81fa68a7c11aed8a3796842a283cffc3
unknown <marcoluigisabini@gmail.com>
2016-03-16 13:43:52 -0400

    Testing DeployHQ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,7 +41,6 @@
 		<div class="container">
 			<div class="content">
 				<div class="title">Kaotic Designs</div>
-				<div class="title">-under construction-</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
